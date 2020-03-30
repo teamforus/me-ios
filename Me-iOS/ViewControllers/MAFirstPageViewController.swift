52a4e523c42701d3338f70eb5e32884de0c485f8
Max Visser <max@forus.io>
2020-03-30 09:19:06 +0200

    Update MAFirstPageViewController.swift

--- Me-iOS/ViewControllers/MAFirstPageViewController.swift
+++ Me-iOS/ViewControllers/MAFirstPageViewController.swift
@@ -180,12 +180,8 @@ class MAFirstPageViewController: UIViewController {
                         self?.emailLoginViewModel.initLoginByEmail(email: self?.emailField.text ?? "")
                   
                 }else if statusCode == 500 {
-<<<<<<< HEAD
                     self?.showSimpleAlertWithSingleAction(title: "Error!".localized(), message: "", okAction: UIAlertAction(title: "OK", style: .default, handler: { (action) in
                     }))
-=======
-
->>>>>>> origin/release/v0.3.0
                 }else {
                     self?.performSegue(withIdentifier: "goToSuccessMail", sender: nil)
                 }
