3ce627f9d0d9d5c0eae3ae07639a70d581edaf47
Daniel Tcacenco <daniel@forus.io>
2021-02-18 13:40:53 +0200

    product voucher part three

--- Me-iOS/ViewControllers/ProductVoucher/ProductVoucherCell/MInfoVoucherTableViewCell.swift
+++ Me-iOS/ViewControllers/ProductVoucher/ProductVoucherCell/MInfoVoucherTableViewCell.swift
@@ -9,75 +9,73 @@
 import UIKit
 
 class MInfoVoucherTableViewCell: UITableViewCell {
-  static let identifier = "MInfoVoucherTableViewCell"
-  
-  private let bodyView: UIView = {
-    let bodyView = UIView(frame: .zero)
-    bodyView.backgroundColor = .white
-    return bodyView
-  }()
-  
-  private let emailButton: ActionButton = {
-    let button = ActionButton(frame: .zero)
-    button.backgroundColor = #colorLiteral(red: 0.9449954033, green: 0.9451572299, blue: 0.9449852109, alpha: 1)
-    return button
-  }()
-  
-  private let voucherInfoButton: ActionButton = {
-    let button = ActionButton(frame: .zero)
-    button.backgroundColor = #colorLiteral(red: 0.9449954033, green: 0.9451572299, blue: 0.9449852109, alpha: 1)
-    return button
-  }()
-  
-  
-  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-    super.init(style: style, reuseIdentifier: reuseIdentifier)
-    addSubviews()
-    setupConstraints()
-    contentView.backgroundColor = .clear
-    self.backgroundColor = .clear
-  }
-
-  required init?(coder: NSCoder) {
-    super.init(coder: coder)
-  }
-  
-
+    static let identifier = "MInfoVoucherTableViewCell"
+    
+    private let bodyView: UIView = {
+        let bodyView = UIView(frame: .zero)
+        bodyView.backgroundColor = .white
+        return bodyView
+    }()
+    
+    private let emailButton: ActionButton = {
+        let button = ActionButton(frame: .zero)
+        button.backgroundColor = #colorLiteral(red: 0.9449954033, green: 0.9451572299, blue: 0.9449852109, alpha: 1)
+        return button
+    }()
+    
+    private let voucherInfoButton: ActionButton = {
+        let button = ActionButton(frame: .zero)
+        button.backgroundColor = #colorLiteral(red: 0.9449954033, green: 0.9451572299, blue: 0.9449852109, alpha: 1)
+        return button
+    }()
+    
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        addSubviews()
+        setupConstraints()
+        contentView.backgroundColor = .clear
+        self.backgroundColor = .clear
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    
 }
 
 extension MInfoVoucherTableViewCell {
-  func addSubviews() {
-    let views = [bodyView, emailButton, voucherInfoButton]
-    views.forEach { (view) in
-      view.translatesAutoresizingMaskIntoConstraints = false
-      self.contentView.addSubview(view)
+    func addSubviews() {
+        let views = [bodyView, emailButton, voucherInfoButton]
+        views.forEach { (view) in
+            view.translatesAutoresizingMaskIntoConstraints = false
+            self.contentView.addSubview(view)
+        }
     }
-  }
-  func setupConstraints(){
-    NSLayoutConstraint.activate([
-      bodyView.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: 0),
-      bodyView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: 0),
-      bodyView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant:  0),
-      bodyView.heightAnchor.constraint(equalToConstant: 70),
-      bodyView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor, constant: 0)
-    ])
+    func setupConstraints(){
+        NSLayoutConstraint.activate([
+            bodyView.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: 0),
+            bodyView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: 0),
+            bodyView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant:  0),
+            bodyView.heightAnchor.constraint(equalToConstant: 70),
+            bodyView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor, constant: 0)
+        ])
+        
+        NSLayoutConstraint.activate([
+            emailButton.topAnchor.constraint(equalTo: self.bodyView.topAnchor, constant: 11),
+            emailButton.leadingAnchor.constraint(equalTo: self.bodyView.leadingAnchor, constant: 20),
+            emailButton.bottomAnchor.constraint(equalTo: self.bodyView.bottomAnchor, constant: -12.5),
+            emailButton.heightAnchor.constraint(equalToConstant: 46),
+        ])
+        
+        NSLayoutConstraint.activate([
+            voucherInfoButton.topAnchor.constraint(equalTo: self.bodyView.topAnchor, constant: 11),
+            voucherInfoButton.trailingAnchor.constraint(equalTo: self.bodyView.trailingAnchor, constant:  -17),
+            voucherInfoButton.centerYAnchor.constraint(equalTo: self.emailButton.centerYAnchor),
+            voucherInfoButton.leadingAnchor.constraint(equalTo: self.emailButton.trailingAnchor, constant: 12),
+            voucherInfoButton.heightAnchor.constraint(equalToConstant: 46),
+        ])
+    }
     
-    NSLayoutConstraint.activate([
-      emailButton.topAnchor.constraint(equalTo: self.bodyView.topAnchor, constant: 11),
-      emailButton.leadingAnchor.constraint(equalTo: self.bodyView.leadingAnchor, constant: 20),
-      emailButton.widthAnchor.constraint(equalToConstant: 180),
-      emailButton.bottomAnchor.constraint(equalTo: self.bodyView.bottomAnchor, constant: -12.5),
-      emailButton.heightAnchor.constraint(equalToConstant: 46),
-      emailButton.widthAnchor.constraint(equalToConstant: 156),
-    ])
-    
-    NSLayoutConstraint.activate([
-      voucherInfoButton.topAnchor.constraint(equalTo: self.bodyView.topAnchor, constant: 11),
-      voucherInfoButton.trailingAnchor.constraint(equalTo: self.bodyView.trailingAnchor, constant:  -17),
-      voucherInfoButton.centerYAnchor.constraint(equalTo: self.emailButton.centerYAnchor),
-      voucherInfoButton.heightAnchor.constraint(equalToConstant: 46),
-      voucherInfoButton.widthAnchor.constraint(equalToConstant: 156),
-    ])
-  }
-  
 }
--- Me-iOS/ViewControllers/ProductVoucher/ProductVoucherViewController.swift
+++ Me-iOS/ViewControllers/ProductVoucher/ProductVoucherViewController.swift
@@ -9,186 +9,178 @@
 import UIKit
 
 enum MainTableViewSection: Int, CaseIterable {
-  case voucher = 0
-  case infoVoucher = 1
-  case mapDetail = 2
-  case adress = 3
-  case telephone = 4
-  case branches = 5
+    case voucher = 0
+    case infoVoucher = 1
+    case mapDetail = 2
+    case adress = 3
+    case telephone = 4
+    case branches = 5
 }
 
 class ProductVoucherViewController: UIViewController {
-  
-  // MARK: - Properties
-  
-  private let backButton: ActionButton = {
-    let button = ActionButton(frame: .zero)
-    button.setImage(#imageLiteral(resourceName: "back"), for: .normal)
-    return button
-  }()
-  
-  private let titleLabel: UILabel = {
-    let label = UILabel(frame: .zero)
-    label.text = "Product Voucher"
-    label.font = R.font.googleSansMedium(size: 17)
-    label.textColor = .black
-    return label
-  }()
-  
-  private let tableView: UITableView = {
-    let tableView = UITableView(frame: .zero)
-    tableView.backgroundColor = .clear
-    tableView.separatorStyle = .none
-    tableView.showsVerticalScrollIndicator = false
-    return tableView
-  }()
-  
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    self.view.backgroundColor = #colorLiteral(red: 0.9646012187, green: 0.9647662044, blue: 0.9645908475, alpha: 1)
-    addSubviews()
-    setUpTableView()
-    addCosntrains()
-    setUpActions()
-  }
-  
-  
-  
-  
-  private func setUpTableView() {
-    tableView.delegate = self
-    tableView.dataSource = self
-    tableView.register(MProductVoucherTableViewCell.self, forCellReuseIdentifier: MProductVoucherTableViewCell.identifier)
-    tableView.register(MTelephoneTableViewCell.self, forCellReuseIdentifier: MTelephoneTableViewCell.identifier)
-    tableView.register(MBranchesTableViewCell.self, forCellReuseIdentifier: MBranchesTableViewCell.identifier)
-    tableView.register(MAdressTableViewCell.self, forCellReuseIdentifier: MAdressTableViewCell.identifier)
-    tableView.register(MMapDetailsTableViewCell.self, forCellReuseIdentifier: MMapDetailsTableViewCell.identifier)
-    tableView.register(MMapDetailsTableViewCell.self, forCellReuseIdentifier: MMapDetailsTableViewCell.identifier)
-    tableView.register(MInfoVoucherTableViewCell.self, forCellReuseIdentifier: MInfoVoucherTableViewCell.identifier)
     
-  }
-  
- 
-  
+    // MARK: - Properties
+    
+    private let backButton: ActionButton = {
+        let button = ActionButton(frame: .zero)
+        button.setImage(#imageLiteral(resourceName: "back"), for: .normal)
+        return button
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.text = "Product Voucher"
+        label.font = R.font.googleSansMedium(size: 17)
+        label.textColor = .black
+        return label
+    }()
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView(frame: .zero)
+        tableView.backgroundColor = .clear
+        tableView.separatorStyle = .none
+        tableView.showsVerticalScrollIndicator = false
+        return tableView
+    }()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = #colorLiteral(red: 0.9646012187, green: 0.9647662044, blue: 0.9645908475, alpha: 1)
+        addSubviews()
+        setUpTableView()
+        addCosntrains()
+        setUpActions()
+    }
+    
+    private func setUpTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(MProductVoucherTableViewCell.self, forCellReuseIdentifier: MProductVoucherTableViewCell.identifier)
+        tableView.register(MTelephoneTableViewCell.self, forCellReuseIdentifier: MTelephoneTableViewCell.identifier)
+        tableView.register(MBranchesTableViewCell.self, forCellReuseIdentifier: MBranchesTableViewCell.identifier)
+        tableView.register(MAdressTableViewCell.self, forCellReuseIdentifier: MAdressTableViewCell.identifier)
+        tableView.register(MMapDetailsTableViewCell.self, forCellReuseIdentifier: MMapDetailsTableViewCell.identifier)
+        tableView.register(MMapDetailsTableViewCell.self, forCellReuseIdentifier: MMapDetailsTableViewCell.identifier)
+        tableView.register(MInfoVoucherTableViewCell.self, forCellReuseIdentifier: MInfoVoucherTableViewCell.identifier)
+    }
 }
 
 extension ProductVoucherViewController: UITableViewDelegate, UITableViewDataSource {
-  
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return MainTableViewSection.allCases.count
-  }
-  
-  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let sections = MainTableViewSection.allCases[indexPath.row]
-    switch sections {
-    case .voucher:
-      guard let cell = tableView.dequeueReusableCell(withIdentifier: MProductVoucherTableViewCell.identifier, for: indexPath) as? MProductVoucherTableViewCell else {
-        return UITableViewCell()
-      }
-      return cell
-    case .infoVoucher:
-      guard let cell = tableView.dequeueReusableCell(withIdentifier: MInfoVoucherTableViewCell.identifier, for: indexPath) as? MInfoVoucherTableViewCell else {
-        return UITableViewCell()
-      }
-return cell
-    case .mapDetail:
-      guard let cell = tableView.dequeueReusableCell(withIdentifier: MAdressTableViewCell.identifier, for: indexPath) as? MAdressTableViewCell else {
-        return UITableViewCell()
-      }
-      return cell
-      
-    case .adress:
-      guard let cell = tableView.dequeueReusableCell(withIdentifier: MTelephoneTableViewCell.identifier, for: indexPath) as? MTelephoneTableViewCell else {
-        return UITableViewCell()
-      }
-      return cell
-      
-    case .telephone:
-      guard let cell = tableView.dequeueReusableCell(withIdentifier: MBranchesTableViewCell.identifier, for: indexPath) as? MBranchesTableViewCell else {
-        return UITableViewCell()
-      }
-      return cell
-      
-    case .branches:
-      guard let cell = tableView.dequeueReusableCell(withIdentifier: MBranchesTableViewCell.identifier, for: indexPath) as? MBranchesTableViewCell else {
-        return UITableViewCell()
-      }
-      return cell
-      
+    // MARK: - UITableViewDelegate
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return MainTableViewSection.allCases.count
     }
-  }
-  
-  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-    let sections = MainTableViewSection.allCases[indexPath.row]
     
-    switch sections {
-    case .voucher:
-      return 120
-    case .infoVoucher:
-      return 85
-    case .mapDetail:
-      return 160 * 12
-    case .adress:
-      return 70
-    case .telephone:
-      return 80
-    case .branches:
-      return 80
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let sections = MainTableViewSection.allCases[indexPath.row]
+        switch sections {
+        case .voucher:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: MProductVoucherTableViewCell.identifier, for: indexPath) as? MProductVoucherTableViewCell else {
+                return UITableViewCell()
+            }
+            return cell
+        case .infoVoucher:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: MInfoVoucherTableViewCell.identifier, for: indexPath) as? MInfoVoucherTableViewCell else {
+                return UITableViewCell()
+            }
+            return cell
+        case .mapDetail:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: MAdressTableViewCell.identifier, for: indexPath) as? MAdressTableViewCell else {
+                return UITableViewCell()
+            }
+            return cell
+            
+        case .adress:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: MTelephoneTableViewCell.identifier, for: indexPath) as? MTelephoneTableViewCell else {
+                return UITableViewCell()
+            }
+            return cell
+            
+        case .telephone:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: MBranchesTableViewCell.identifier, for: indexPath) as? MBranchesTableViewCell else {
+                return UITableViewCell()
+            }
+            return cell
+            
+        case .branches:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: MBranchesTableViewCell.identifier, for: indexPath) as? MBranchesTableViewCell else {
+                return UITableViewCell()
+            }
+            return cell
+            
+        }
     }
-  }
-  
-  
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        let sections = MainTableViewSection.allCases[indexPath.row]
+        
+        switch sections {
+        case .voucher:
+            return 120
+        case .infoVoucher:
+            return 85
+        case .mapDetail:
+            return 160
+        case .adress:
+            return 70
+        case .telephone:
+            return 80
+        case .branches:
+            return 80
+        }
+    }
+    
+    
 }
 
 extension ProductVoucherViewController {
-  
-  func addSubviews(){
-    let views = [tableView, backButton, titleLabel]
-    views.forEach { (view) in
-      view.translatesAutoresizingMaskIntoConstraints = false
-      self.view.addSubview(view)
+    
+    // MARK: - Add Subviews
+    private func addSubviews(){
+        let views = [tableView, backButton, titleLabel]
+        views.forEach { (view) in
+            view.translatesAutoresizingMaskIntoConstraints = false
+            self.view.addSubview(view)
+        }
     }
-  }
 }
 
 extension ProductVoucherViewController{
-  // MARK: - Add Constraints
-  
-  func addCosntrains(){
-    NSLayoutConstraint.activate([
-      tableView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 103),
-      tableView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 0),
-      tableView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: 0),
-      tableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: 0)
-    ])
     
-    NSLayoutConstraint.activate([
-      backButton.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 54),
-      backButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 14),
-      backButton.heightAnchor.constraint(equalToConstant: 44),
-      backButton.widthAnchor.constraint(equalToConstant: 44),
-    ])
-    
-    NSLayoutConstraint.activate([
-      titleLabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 56),
-      titleLabel.leadingAnchor.constraint(equalTo: self.backButton.leadingAnchor, constant: 84),
-     titleLabel.centerYAnchor.constraint(equalTo: self.backButton.centerYAnchor),
-     titleLabel.widthAnchor.constraint(equalToConstant: 200),
-    ])
-    
-    
-  }
+    // MARK: - Add Constraints
+    private func addCosntrains(){
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 103),
+            tableView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 0),
+            tableView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: 0),
+            tableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: 0)
+        ])
+        
+        NSLayoutConstraint.activate([
+            backButton.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 54),
+            backButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 14),
+            backButton.heightAnchor.constraint(equalToConstant: 44),
+            backButton.widthAnchor.constraint(equalToConstant: 44),
+        ])
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 56),
+            titleLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            titleLabel.centerYAnchor.constraint(equalTo: self.backButton.centerYAnchor),
+            titleLabel.widthAnchor.constraint(equalToConstant: 200),
+        ])
+        
+    }
 }
 
 
 extension ProductVoucherViewController{
-  
-  //MARK:- Setup Actions
-  
-  private func setUpActions(){
-    backButton.actionHandleBlock = { [weak self] (button) in
-      self?.back(button)
+    
+    //MARK: - Setup Actions
+    private func setUpActions(){
+        backButton.actionHandleBlock = { [weak self] (button) in
+            self?.back(button)
+        }
     }
-  }
 }
