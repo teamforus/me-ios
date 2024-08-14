d53f9ab5e0495e4591c67e6680be511096905988
Daniel Tcacenco <daniel.tcacenco@gmail.com>
2024-08-14 10:22:03 +0300

    improvements

--- Me-iOS/ViewControllers/ProductVoucher/ProductVoucherCell/MProductVoucherTableViewCell.swift
+++ Me-iOS/ViewControllers/ProductVoucher/ProductVoucherCell/MProductVoucherTableViewCell.swift
@@ -72,7 +72,7 @@ class MProductVoucherTableViewCell: UITableViewCell {
         
         if voucher.fund?.type == FundType.subsidies.rawValue {
             self.priceLabel.isHidden = true
-        }else if voucher.amount_visible {
+        }else if voucher.amount_visible ?? false{
             self.priceLabel.isHidden = true
         }
       
